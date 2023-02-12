0fc7500240fb812f8da715fe4376f9dc3cf2d5c8
DavidLegarre <davidls920@gmail.com>
2023-02-12 22:52:24 +0100

    Added button

--- EbookReader/src/main.rs
+++ EbookReader/src/main.rs
@@ -1,26 +1,41 @@
-use glib::clone;
-// glib and other dependencies are re-exported by the gtk crate
-use gtk::glib;
 use gtk::prelude::*;
+use gtk::{Application, ApplicationWindow, Button};
 
-// When the application is launched…
-fn on_activate(application: &gtk::Application) {
-    // … create a new window …
-    let window = gtk::ApplicationWindow::new(application);
-    // … with a button in it …
-    let button = gtk::Button::with_label("Hello World!");
-    // … which closes the window when clicked
-    button.connect_clicked(clone!(@weak window => move |_| window.close()));
-    window.set_child(Some(&button));
-    window.present();
-}
+const APP_ID: &str = "org.gtx_rs.HellowWorld1";
 
 fn main() {
-    // Create a new application with the builder pattern
-    let app = gtk::Application::builder()
-        .application_id("com.github.gtk-rs.examples.basic")
-        .build();
-    app.connect_activate(on_activate);
+    // Create a new application
+    let app = Application::builder().application_id(APP_ID).build();
+
+    // Conect to "activate" signal of `app`
+    app.connect_activate(build_ui);
+
     // Run the application
     app.run();
+}
+
+fn build_ui(app: &Application) {
+    // Creayte a button with label margins
+    let button = Button::builder()
+        .label("Press me!")
+        .margin_top(12)
+        .margin_bottom(12)
+        .margin_start(12)
+        .margin_end(12)
+        .build();
+
+    // Connect to "clicked" signal of `button`
+    button.connect_clicked(|button| {
+        // Set the label to "Hello world!" after the button has been clicked
+        button.set_label("Hello World!");
+    });
+    // Create a windows set the title
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("My GTK App")
+        .child(&button)
+        .build();
+
+    // Present window
+    window.present();
 }
